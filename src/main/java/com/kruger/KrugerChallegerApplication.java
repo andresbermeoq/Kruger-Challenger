6ec5125348cc4f8ead26c596b3c7d928aebadba0
Andres Bermeo <andresbermeoq1@gmail.com>
2022-08-18 12:54:11 -0500

    Inicial Commit - Creation Project

---
+++ src/main/java/com/kruger/KrugerChallegerApplication.java
@@ -0,0 +1,13 @@
+package com.kruger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrugerChallegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrugerChallegerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kruger/KrugerChallegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrugerChallegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
